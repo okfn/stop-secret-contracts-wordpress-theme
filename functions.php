a085610c86e221838d10007757ab1ee752870be9
Sam Smith <sam@mintcanary.com>
2014-06-10 14:27:25 +0100

    add sidebar template

--- functions.php
+++ functions.php
@@ -60,17 +60,17 @@ you like. Enjoy!
 
 // Sidebars & Widgetizes Areas
 function wp_bootstrap_register_sidebars() {
-    /*
+    
     register_sidebar(array(
     	'id' => 'sidebar1',
-    	'name' => 'Main Sidebar',
-    	'description' => 'Used on every page BUT the homepage page template.',
+    	'name' => 'Sidebar',
+    	'description' => 'Used on every with the sidebar template.',
     	'before_widget' => '<div id="%1$s" class="widget %2$s">',
     	'after_widget' => '</div>',
     	'before_title' => '<h4 class="widgettitle">',
     	'after_title' => '</h4>',
     ));
-    
+    /*
     register_sidebar(array(
     	'id' => 'sidebar2',
     	'name' => 'Homepage Sidebar',
